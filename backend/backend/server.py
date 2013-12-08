e5f722458aab406b5eb25d3e3498fef319df7f86
batterivatten <frazor@gmail.com>
2013-12-08 23:52:39 +0100

    Initial backend service
    Signed-off-by: batterivatten <frazor@gmail.com>

---
+++ backend/backend/server.py
@@ -0,0 +1,18 @@
+#!env python
+
+#from tornado import ioloop
+import logging, sys
+
+class server():
+	"""
+	>>> survur = server()
+	True
+	"""
+
+	def __init__(self):
+		pass
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
---
+++ backend/runtests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PYTHONPATH=$(basename $0)
+
+python backend/server.py
