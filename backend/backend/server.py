c46aff532420b3704e8f11e9d085b75f81853c18
batterivatten <frazor@gmail.com>
2013-12-09 00:43:11 +0100

    rabbitMQ connection halfway setup
    Signed-off-by: batterivatten <frazor@gmail.com>

--- backend/backend/server.py
+++ backend/backend/server.py
@@ -1,15 +1,22 @@
 #!env python
 
 import tornado.ioloop
-import logging, sys
+import logging, sys, pika
 
-class server():
+class HostBackend():
 	"""
-	>>> survur = server()
+	>>> ioloop = tornado.ioloop.IOLoop()
+	>>> survur = HostBackend(ioloop)
 	True
 	"""
 
-	def __init__(self):
+	def __init__(self, ioloop):
+		self.ioloop = ioloop
+		self.rabbitcredentials = pika.PlainCredentials('username', 'password')
+		self.rabbitparams = pika.ConnectionParameters('localhost', 5672, '/', self.rabbitcredentials)
+		self.pikaconn = pika.SelectConnection(self.rabbitparams, self.on_pika_connect)
+
+	def on_pika_connect(self, connection):
 		pass
 
 if __name__ == "__main__":
