c47e0bdb134900013e16fce049e18412174c943c
mjukis <swede@wccorp.org>
2014-10-20 14:11:30 -0700

    Update looptest.py
    Adds a nonblocking keyboard grabber

--- frontend/looptest.py
+++ frontend/looptest.py
@@ -18,8 +18,6 @@ def xstr(s):
 
 def draw_background(win):
     win.erase()
-#    topstring = wccutil
-#    bottomstring = operator + " @ " + machine
     topstring = "WASTECOMM NET TERMINAL"
     bottomstring = "WCC#657"
     bottomfillstring = (78 - len(bottomstring)) * " "
@@ -37,7 +35,7 @@ def get_datetime():
     timeoutput = time.strftime("%d %b %Y %H:%M:%S",currdatetime)
 
 def write_datetime(win):
-    #separate function since this gets done A LOT
+    #let's write that pretty datetime
     get_datetime()
     win.move(0,59)
     win.clrtoeol()
@@ -46,20 +44,36 @@ def write_datetime(win):
     win.refresh()
 
 def task():
+    #function that calls the datetime-writer
     write_datetime(stdscr)
 
 def task2():
+    #function that proves async by writing RAGH
     win = stdscr
     win.move(2,3)
     win.addstr("RAGH")
 
 def task3():
+    #function that kills the program cleanly after a set time
+    #this needs to be replaced by an ending to a proper main loop
+    #so unexpected interruptions are handled without fucking
+    #the terminal
     win = stdscr
     stdscr.keypad(0)
     curses.echo()
     curses.nocbreak()
     curses.endwin()
     exit()
+
+def task4():
+    #function that reads and displays keypresses
+    win = stdscr
+    inch = win.getch()
+    if inch > -1:
+        win.move(3,3)
+        win.addstr("      ")
+        win.move(3,3)
+        win.addstr(xstr(inch))
 
 if __name__ == "__main__":
     try:
@@ -73,6 +87,8 @@ if __name__ == "__main__":
         # where no buffering is performed on keyboard input
         curses.noecho()
         curses.cbreak()
+        #next line makes getch() nonblocking
+        stdscr.nodelay(1)
         # In keypad mode, escape sequences for special keys
         # (like the cursor keys) will be interpreted and
         # a special value like curses.KEY_LEFT will be returned
@@ -83,8 +99,13 @@ if __name__ == "__main__":
         curses.echo()
         curses.nocbreak()
         curses.endwin()
-        #traceback.print_exc()           # Print the exception
+
     draw_background(stdscr)
+    #the following line is kinda shitty because it loops an
+    #input grab even though the input grab is not blocking,
+    #which sort of simulates a laggy keyboard but should be a
+    #normal callback for menus maybe?
+    PeriodicCallback(task4, 100).start()
     PeriodicCallback(task, 1000).start()
     PeriodicCallback(task2, 3000).start()
     PeriodicCallback(task3, 10000).start()
